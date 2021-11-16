0a06b9b0efbb13a335ea8a5ea2f21567035873eb
hiepnvhn2000 <hiepnvhn2000@gmail.com>
2021-11-17 00:53:01 +0700

    Initial commit

---
+++ src/app/tabs/tab-group.component.ts
@@ -0,0 +1,42 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { TabPanelComponent } from './tab-panel.component';
+
+@Component({
+  selector: 'tab-group',
+  template: `
+    <div class="tab-headers">
+      <div class="tab-header-item" *ngFor="let tab of tabPanelList; let idx = index" (click)="activeIndexChange.emit(idx)">
+        {{tab.title}}
+        <button (click)="removeTab(tab)">x</button>
+      </div>
+    </div>
+    <div class="tab-body" *ngIf="tabPanelList.length;else noTabs">
+      <ng-container *ngTemplateOutlet="tabPanelList[activeIndex].panelBody"></ng-container>
+    </div>
+    <ng-template #noTabs>No more tabs.</ng-template>
+  `,
+})
+export class TabGroupComponent {
+  tabPanelList: TabPanelComponent[] = [];
+  @Input() activeIndex = 0;
+  @Output() activeIndexChange = new EventEmitter<number>();
+
+  addTab(tab: TabPanelComponent) {
+    this.tabPanelList = [...this.tabPanelList, tab];
+  }
+  removeTab(tab: TabPanelComponent) {
+    let found = -1;
+    this.tabPanelList = this.tabPanelList.filter((tp, index) => {
+      if (tp === tab) {
+        found = index;
+        return false;
+      }
+      return true;
+    });
+    if (found === this.activeIndex) {
+      this.activeIndexChange.emit(
+        found === this.tabPanelList.length ? found - 1 : found
+      );
+    }
+  }
+}
